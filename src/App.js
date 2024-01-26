5d126b0b191e6cc41fb674b31f51bb6e37adb524
MeilingM <violinmeiling@gmail.com>
2024-01-26 10:53:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey
+          Sort Your Music
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button">Log in with Spotify</button>
       </header>
     </div>
   );
