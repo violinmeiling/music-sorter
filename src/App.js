2c763249c8fc6162edd7436cc85bc912b8b5ed09
MeilingM <violinmeiling@gmail.com>
2024-01-26 13:24:36 -0500

    create spotify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const CLIENT_ID = "c5a71d998c634176b0dc265461524ede"
+  const REDIRECT_URI = "https://violinmeiling.github.io/music-sorter/"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+}, [])
+
+const logout = () => {
+  setToken("")
+  window.localStorage.removeItem("token")
+}
+
+const [searchKey, setSearchKey] = useState("")
+const [artists, setArtists] = useState([])
+
+const searchArtists = async (e) => {
+  e.preventDefault()
+  const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },
+      params: {
+          q: searchKey,
+          type: "artist"
+      }
+  })
+
+  setArtists(data.artists.items)
+}
+
+const renderArtists = () => {
+  return artists.map(artist => (
+      <div key={artist.id}>
+          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+          {artist.name}
+      </div>
+  ))
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
           Sort Your Music
+        <p>
+          Sort your music into playlists based on their properties. Log in below:
         </p>
-        <button type="button">Log in with Spotify</button>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}><button class='btn'>
+  Log in with Spotify
+  <img src={require('./Spotify.png')} height="10%" width="10%"/>
+</button>
+</a>
+
+<form onSubmit={searchArtists}>
+    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+    <button type={"submit"}>Search</button>
+</form>
+
+{renderArtists()}
+
+
       </header>
     </div>
   );
